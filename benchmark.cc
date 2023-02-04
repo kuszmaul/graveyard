aad98adf8ca726c194efda1d3793f75e44c3cad9
Bradley Kuszmaul <kuszmaul@gmail.com>
2023-02-04 09:43:39 -0500

    Standard deviation

--- benchmark.cc
+++ benchmark.cc
@@ -2,8 +2,10 @@
 
 #include <time.h>
 
+#include <cmath>
 #include <cstdint>
 #include <functional>
+#include <iostream>
 
 namespace {
 inline uint64_t operator-(struct timespec a, struct timespec b) {
@@ -13,13 +15,31 @@ inline uint64_t operator-(struct timespec a, struct timespec b) {
 
 void Benchmark(std::ofstream& output, std::function<void(size_t count)> setup,
                std::function<size_t()> fun, const std::vector<size_t>& counts) {
+  constexpr size_t kNumberOfTrials = 5;
+  constexpr double kNInverse = 1.0 / kNumberOfTrials;
   for (size_t count : counts) {
     setup(count);
-    struct timespec start;
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    size_t memory_size = fun();
-    struct timespec end;
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    output << count << "," << end - start << "," << memory_size << std::endl;
+    uint64_t x_sum = 0;
+    uint64_t x_squared_sum = 0;
+    size_t memory_size;
+    (std::cerr << " " << count).flush();
+    for (size_t trial = 0; trial < kNumberOfTrials; ++trial) {
+      struct timespec start;
+      clock_gettime(CLOCK_MONOTONIC, &start);
+      memory_size = fun();
+      struct timespec end;
+      clock_gettime(CLOCK_MONOTONIC, &end);
+      uint64_t elapsed = end - start;
+      x_sum += elapsed;
+      x_squared_sum += elapsed * elapsed;
+      output << "#" << count << "," << elapsed << "," << memory_size << std::endl;
+      (std::cerr << ".").flush();
+    }
+    // These variables are named as if in Reverse Polish Notation.
+    double x_expected = x_sum * kNInverse;
+    double x_expected_square = x_expected * x_expected;
+    double x_square_expected = x_squared_sum * kNInverse;
+    double standard_deviation = sqrt(x_square_expected - x_expected_square);
+    output << count << "," << x_expected << "," << memory_size << "," << 2 * standard_deviation << std::endl;
   }
 }

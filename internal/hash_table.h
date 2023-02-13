64268df9c76fc2ae8295fd1f0317ece24834e519
Bradley Kuszmaul <kuszmaul@gmail.com>
2023-02-13 09:49:41 -0500

    Moved the implementation into `HashTable` which takes a single `Traits` template parameter.

--- internal/hash_table.h
+++ internal/hash_table.h
@@ -5,13 +5,175 @@
 
 namespace yobiduck::internal {
 
+// Returns the ceiling of (a/b).
+inline constexpr size_t ceil(size_t a, size_t b) { return (a + b - 1) / b; }
+
 template <class KeyType, class MappedTypeOrVoid, class Hash, class KeyEqual, class Allocator>
 struct HashTableTraits {
   using key_type = KeyType;
-  using mapped_type_or_void = MappedTypeOrVoid;  
+  using mapped_type_or_void = MappedTypeOrVoid;
+  using value_type = typename std::conditional<std::is_same<mapped_type_or_void, void>::value,
+                                               key_type,
+                                               std::pair<const key_type, mapped_type_or_void>>::type;
   using hasher = Hash;
   using key_equal = KeyEqual;
   using allocator = Allocator;
+  static constexpr size_t kSlotsPerBucket = 14;
+  // H2 Value for empty slots
+  static constexpr uint8_t kEmpty = 255;
+  static constexpr uint8_t kSearchDistanceEndSentinal = 255;
+  static constexpr size_t kCacheLineSize = 64;
+};
+
+template <class Traits>
+union Item {
+  char bytes[sizeof(typename Traits::value_type)];
+  typename Traits::value_type value;
+};
+
+template <class Traits>
+struct Bucket {
+  // Bucket has no constructor or destructor since by default it's POD.
+  Bucket() = delete;
+  // Copy constructor
+  Bucket(const Bucket&) = delete;
+  // Copy assignment
+  Bucket& operator=(Bucket other) = delete;
+  // Move constructor
+  Bucket(Bucket&& other) = delete;
+  // Move assignment
+  Bucket& operator=(Bucket&& other) = delete;
+  // Destructor
+  ~Bucket() = delete;
+
+  void Init() {
+    search_distance = 0;
+    for (size_t i = 0; i < Traits::kSlotsPerBucket; ++i) h2[i] = Traits::kEmpty;
+  }
+
+
+  std::array<uint8_t, Traits::kSlotsPerBucket> h2;
+  // The number of buckets we must search in an unsuccessful lookup that starts
+  // here.
+  uint8_t search_distance;
+  std::array<Item<Traits>, Traits::kSlotsPerBucket> slots;
+};
+
+template <class Traits>
+class Buckets {
+ public:
+  // Constructs a `Buckets` with size 0 and no allocated memory.
+  Buckets() = default;
+  // Copy constructor
+  Buckets(const Buckets&) = delete;
+  // Copy assignment
+  Buckets& operator=(Buckets other) = delete;
+  // Move constructor
+  Buckets(Buckets&& other) = delete;
+  // Move assignment
+  Buckets& operator=(Buckets&& other) = delete;
+
+  ~Buckets() {
+    assert((physical_size_ == 0) == (buckets_ == nullptr));
+    if (buckets_ != nullptr) {
+      // TODO: Destruct the nonempty slots
+      free(buckets_);
+      buckets_ = nullptr;
+    }
+  }
+
+  // Constructs a `Buckets` that has the given logical bucket size (which must
+  // be positive).
+  explicit Buckets(size_t logical_size) : logical_size_(logical_size) {
+    assert(logical_size_ > 0);
+    // Add 4 buckets if logical_size_ > 4.
+    // Add 3 buckets if logical_size_ == 4.
+    // Add 2 buckets if logical_size_ == 3.
+    // Add 1 bucket if logical_bucket_count_ <= 2.
+    size_t extra_buckets = (logical_size_ > 4)    ? 4
+                           : (logical_size_ <= 2) ? 1
+                           : logical_size_ - 1;
+    physical_size_ = logical_size_ + extra_buckets;
+    assert(physical_size_ > 0);
+    // TODO: Round up the physical_bucket_size_ to the actual size allocated.
+    // To do this we can call malloc_usable_size to find out how big it really
+    // is.  But we aren't supposed to modify those bytes (it will mess up
+    // tools such as address sanitizer or valgrind).  So we realloc the
+    // pointer to the actual size.
+    buckets_ = static_cast<Bucket<Traits>*>(
+        aligned_alloc(Traits::kCacheLineSize, physical_size_ * sizeof(*buckets_)));
+    assert(buckets_ != nullptr);
+    for (Bucket<Traits>& bucket : *this) {
+      bucket.Init();
+    }
+    // Set the end-of-search sentinal.
+    buckets_[physical_size_ - 1].search_distance =
+        Traits::kSearchDistanceEndSentinal;
+    if (0) {
+      // It turns out that for libc malloc, the extra usable size usually just
+      // 8 extra bytes.
+      size_t allocated = malloc_usable_size(buckets_);
+      LOG(INFO) << "logical_size=" << logical_size_
+                << " physical_size=" << physical_size_
+                << " allocated bytes=" << allocated
+                << " size=" << allocated / sizeof(*buckets_) << " %"
+                << sizeof(*buckets_) << "=" << allocated % sizeof(*buckets_);
+    }
+  }
+
+  // The method names are lower_case (rather than CamelCase) to mimic a
+  // std::vector.
+
+  void clear() {
+    logical_size_ = 0;
+    physical_size_ = 0;
+    free(buckets_);
+    buckets_ = 0;
+  }
+
+  void swap(Buckets& other) {
+    using std::swap;
+    swap(logical_size_, other.logical_size_);
+    swap(physical_size_, other.physical_size_);
+    swap(buckets_, other.buckets_);
+  }
+
+  size_t logical_size() const { return logical_size_; }
+  size_t physical_size() const { return physical_size_; }
+  bool empty() const { return physical_size() == 0; }
+  Bucket<Traits>& operator[](size_t index) {
+    assert(index < physical_size());
+    return buckets_[index];
+  }
+  const Bucket<Traits>& operator[](size_t index) const {
+    assert(index < physical_size());
+    return buckets_[index];
+  }
+  Bucket<Traits>* begin() { return buckets_; }
+  const Bucket<Traits>* begin() const { return buckets_; }
+  const Bucket<Traits>* cbegin() const { return buckets_; }
+  Bucket<Traits>* end() { return buckets_ + physical_size_; }
+  const Bucket<Traits>* end() const { return buckets_ + physical_size_; }
+  const Bucket<Traits>* cend() const { return buckets_ + physical_size_; }
+
+  // Returns the preferred bucket number, also known as the H1 hash.
+  size_t H1(size_t hash) const {
+    // TODO: Use the absl version.
+    return size_t((__int128(hash) * __int128(logical_size())) >> 64);
+  }
+
+  // Returns the H2 hash, used by vector instructions to filter out most of the
+  // not-equal entries.
+  size_t H2(size_t hash) const { return hash % 255; }
+
+ private:
+  // For computing the index from the hash.  The actual buckets vector is longer
+  // (`physical_size_`) so that we can overflow simply by going off the end.
+  size_t logical_size_ = 0;
+  // The length of `buckets_`, as allocated.
+  // TODO: Put the physical size into the malloced memory.
+  size_t physical_size_ = 0;
+  Bucket<Traits>* buckets_ = nullptr;
 };
 
 template <class Traits>
@@ -26,9 +188,7 @@ class HashTable :
 
  public:
   using key_type = typename Traits::key_type;
-  using value_type = typename std::conditional<std::is_same<typename Traits::mapped_type_or_void, void>::value,
-                                               key_type,
-                                               std::pair<const key_type, typename Traits::mapped_type_or_void>>::type;
+  using value_type = typename Traits::value_type;
   // This won't be exported by sets, but will be exported by maps.
   using mapped_type = typename Traits::mapped_type_or_void;
   using size_type = std::size_t;
@@ -41,23 +201,33 @@ class HashTable :
   using const_reference = const value_type&;
   using pointer = typename std::allocator_traits<allocator_type>::pointer;
   using const_pointer = typename std::allocator_traits<allocator_type>::const_pointer;
-  HashTable() :HashTable(0, hasher(), key_equal(), allocator_type()) {}
-  explicit HashTable(size_t initial_capacity, hasher const& hash = hasher(), key_equal const& key_eq = key_equal(), allocator_type const& allocator = allocator_type()) 
-      :HasherHolder(hash), KeyEqualHolder(key_eq), AllocatorHolder(allocator) {
-    reserve(initial_capacity);
-  }
+  HashTable();
+  explicit HashTable(size_t initial_capacity, hasher const& hash = hasher(), key_equal const& key_eq = key_equal(), allocator_type const& allocator = allocator_type());
   // Copy constructor
-  explicit HashTable(const HashTable& other) :HashTable(other, std::allocator_traits<allocator_type>::select_on_container_copy_construction(get_allocator_ref())) {}
-  HashTable(const HashTable& other, const allocator_type& a) :HashTable(other.size(), other.get_hasher_ref(), other.get_key_eq_ref(), a) {
-    for (const auto& v: other) {
-      insert(v); // TODO: Take advantage of the fact that we know `v` isn't in `*this`.
-    }
-  }
+  explicit HashTable(const HashTable& other);
+  HashTable(const HashTable& other, const allocator_type& a);
 
-  // iterator not here
-  // const_iterator not here
-  // node_type not here
-  // insert_return_type not here
+  class iterator;
+  class const_iterator;
+
+  iterator begin();
+  iterator end();
+  const_iterator begin() const { return cbegin(); }
+  const_iterator end() const { return cend(); }
+  const_iterator cbegin() const;
+  const_iterator cend() const;
+
+  // node_type not implemented
+  // insert_return_type not implemented
+
+  size_t size() const noexcept;
+
+  void clear();
+  bool insert(value_type value);
+  void swap(HashTable& other) noexcept;
+
+  bool contains(const key_type& value) const;
+
   allocator_type get_allocator() const { return get_allocator_ref(); }
   allocator_type& get_allocator_ref() { return *static_cast<AllocatorHolder&>(*this); }
   const allocator_type& get_allocator_ref() const { return *static_cast<AllocatorHolder&>(*this); }
@@ -68,8 +238,311 @@ class HashTable :
   key_equal& get_key_eq_ref() { return *static_cast<KeyEqualHolder&>(*this); }
   const key_equal& get_key_eq_ref() const { return *static_cast<KeyEqualHolder&>(*this); }
 
+  // Returns the actual size of the buckets including the overflow buckets.
+  size_t bucket_count() const { return buckets_.physical_size() * Traits::kSlotsPerBucket; }
+  size_t capacity() const { return bucket_count(); }
+  
+  // size_t GetMemorySize() const;
+  //
+  // Effect: Returns the memory consumed by *this, including the memory in *this
+  // itself, plus any allocated memory.
+  size_t GetMemorySize() const {
+    return sizeof(*this) + buckets_.physical_size() * sizeof(*buckets_.begin());
+  }
+
+
+  // Rehashes the table so that we can hold at least count.
+  void rehash(size_t count);
+
   void reserve(size_t count);
+
+ private:
+  // Ranges from 3/4 full to 7/8 full.
+
+  // Returns true if the table needs rehashing to be big enough to hold
+  // `target_size` elements.
+  bool NeedsRehash(size_t target_size) const;
+
+  // The number of slots that we are aiming for, not counting the overflow slots
+  // at the end.  This value is used to compute the H1 hash (which maps from T
+  // to Z/LogicalSlotCount().)
+  size_t LogicalSlotCount() const;
+
+  // The number of present items in all the buckets combined.
+  // Todo: Put `size_` into buckets_ (in the memory).
+  size_t size_ = 0;
+  Buckets<Traits> buckets_;
 };
+
+template <class Traits>
+HashTable<Traits>::HashTable() :HashTable(0, hasher(), key_equal(), allocator_type()) {}
+
+template <class Traits>
+HashTable<Traits>::HashTable(size_t initial_capacity, hasher const& hash, key_equal const& key_eq, allocator_type const& allocator) 
+      :HasherHolder(hash), KeyEqualHolder(key_eq), AllocatorHolder(allocator) {
+  reserve(initial_capacity);
+}
+
+template <class Traits>
+HashTable<Traits>::HashTable(const HashTable& other) :HashTable(other, std::allocator_traits<allocator_type>::select_on_container_copy_construction(get_allocator_ref())) {}
+
+template <class Traits>
+HashTable<Traits>::HashTable(const HashTable& other, const allocator_type& a) :HashTable(other.size(), other.get_hasher_ref(), other.get_key_eq_ref(), a) {
+  for (const auto& v: other) {
+    insert(v); // TODO: Take advantage of the fact that we know `v` isn't in `*this`.
+  }
+}
+
+template <class Traits>
+typename HashTable<Traits>::iterator HashTable<Traits>::begin() {
+  auto it = iterator(buckets_.begin(), 0);
+  if (!buckets_.empty()) {
+    it.SkipEmpty();
+  }
+  return it;
+}
+
+template <class Traits>
+typename HashTable<Traits>::const_iterator HashTable<Traits>::cbegin() const {
+  auto it = const_iterator(buckets_.cbegin(), 0);
+  if (!buckets_.empty()) {
+    it.SkipEmpty();
+  }
+  return it;
+}
+
+template <class Traits>
+typename HashTable<Traits>::iterator HashTable<Traits>::end() {
+  return iterator(buckets_.end(), 0);
+}
+
+template <class Traits>
+typename HashTable<Traits>::const_iterator HashTable<Traits>::cend() const {
+  return const_iterator(buckets_.cend(), 0);
+}
+
+template <class Traits>
+class HashTable<Traits>::iterator {
+ public:
+  using difference_type = ptrdiff_t;
+  using value_type = typename Traits::value_type;
+  using pointer = value_type*;
+  using reference = value_type&;
+  using iterator_category = std::forward_iterator_tag;
+
+  iterator() = default;
+  iterator& operator++() {
+    ++index_;
+    return SkipEmpty();
+  }
+
+  reference operator*() { return bucket_->slots[index].value; }
+
+ private:
+  friend const_iterator;
+  friend bool operator==(const iterator& a, const iterator& b) {
+    return a.bucket_ == b.bucket_ && a.index_ == b.index_;
+  }
+  friend bool operator!=(const iterator& a, const iterator& b) {
+    return !(a == b);
+  }
+  friend HashTable;
+  // index_ is allowed to be kSlotsPerBucket
+  iterator& SkipEmpty() {
+    while (true) {
+      while (index_ < Traits::kSlotsPerBucket) {
+        if (bucket_->h2[index_] != Traits::kEmpty) {
+          return *this;
+        }
+        ++index_;
+      }
+      bool is_last =
+          bucket_->search_distance == Traits::kSearchDistanceEndSentinal;
+      index_ = 0;
+      ++bucket_;
+      if (is_last) {
+        // *this is the end iterator.
+        return *this;
+      }
+    }
+  }
+  iterator(Bucket<Traits>* bucket, size_t index) : bucket_(bucket), index_(index) {}
+  // The end iterator is represented with bucket_ == buckets_.end()
+  // and index_ == kSlotsPerBucket.
+  Bucket<Traits>* bucket_;
+  size_t index_;
+};
+
+// TODO: Reduce the copy pasta.
+template <class Traits>
+class HashTable<Traits>::const_iterator {
+ public:
+  using difference_type = ptrdiff_t;
+  using value_type = HashTable::value_type;
+  using pointer = const value_type*;
+  using reference = const value_type&;
+  using iterator_category = std::forward_iterator_tag;
+
+  const_iterator() = default;
+  // Implicit constructor
+  const_iterator(iterator x) : bucket_(x.bucket_), index_(x.index_) {}
+  const_iterator& operator++() {
+    ++index_;
+    return SkipEmpty();
+  }
+
+  reference operator*() { return bucket_->slots[index_].value; }
+
+ private:
+  friend bool operator==(const const_iterator& a, const const_iterator& b) {
+    return a.bucket_ == b.bucket_ && a.index_ == b.index_;
+  }
+  friend bool operator!=(const const_iterator& a, const const_iterator& b) {
+    return !(a == b);
+  }
+  friend HashTable;
+  // index_ is allowed to be kSlotsPerBucket
+  const_iterator& SkipEmpty() {
+    while (true) {
+      while (index_ < Traits::kSlotsPerBucket) {
+        if (bucket_->h2[index_] != Traits::kEmpty) {
+          return *this;
+        }
+        ++index_;
+      }
+      bool is_last =
+          bucket_->search_distance == Traits::kSearchDistanceEndSentinal;
+      index_ = 0;
+      ++bucket_;
+      if (is_last) {
+        // *this is the end const_iterator.
+        return *this;
+      }
+    }
+  }
+  const_iterator(const Bucket<Traits>* bucket, size_t index)
+      : bucket_(bucket), index_(index) {}
+  // The end const_iterator is represented with bucket_ == buckets_.end()
+  // and index_ == kSlotsPerBucket.
+  const Bucket<Traits>* bucket_;
+  size_t index_;
+};
+
+template <class Traits>
+size_t HashTable<Traits>::size() const noexcept {
+  return size_;
+}
+
+template <class Traits>
+void HashTable<Traits>::clear() {
+  size_ = 0;
+  buckets_.Clear();
+}
+
+template <class Traits>
+bool HashTable<Traits>::insert(value_type value) {
+  // If (size_ + 1) > 7*8 slot_count_ then rehash.
+  if (NeedsRehash(size_ + 1)) {
+    // rehash to be 3/4 full
+    rehash(ceil((size_ + 1) * 4, 3));
+  }
+  // TODO: Use the Hash here and in OLP.
+  const size_t preferred_bucket = buckets_.H1(value);
+  const size_t h2 = buckets_.H2(value);
+  const size_t distance = buckets_[preferred_bucket].search_distance;
+  for (size_t i = 0; i <= distance; ++i) {
+    assert(preferred_bucket + i < buckets_.physical_size());
+    const Bucket<Traits>& bucket = buckets_[preferred_bucket + i];
+    // TODO: Use vector instructions to replace this loop.
+    for (size_t j = 0; j < Traits::kSlotsPerBucket; ++j) {
+      if (bucket.h2[j] == h2 && bucket.slots[j].value == value) {
+        LOG(INFO) << " Already there in bucket " << preferred_bucket + i;
+        return false;
+      }
+    }
+  }
+  for (size_t i = 0; true; ++i) {
+    assert(preferred_bucket + i < buckets_.physical_size());
+    Bucket<Traits>& bucket = buckets_[preferred_bucket + i];
+    // TODO: Use vector instructions to replace this loop.
+    for (uint8_t j = 0; j < Traits::kSlotsPerBucket; ++j) {
+      if (bucket.h2[j] == Traits::kEmpty) {
+        bucket.h2[j] = h2;
+        // TODO: Construct in place
+        bucket.slots[j].value = value;
+        assert(i < Traits::kSearchDistanceEndSentinal);
+        if (i > buckets_[preferred_bucket].search_distance)
+          buckets_[preferred_bucket].search_distance = i;
+        ++size_;
+        // TODO: Keep track if we are allowed to destabilize pointers, and if we
+        // are, move things around to be sorted.
+        return true;
+      }
+    }
+  }
+}
+
+template <class Traits>
+void HashTable<Traits>::swap(HashTable& other) noexcept {
+  std::swap(size_, other.size_);
+  buckets_.swap(other.buckets_);
+}
+
+template <class Traits>
+bool HashTable<Traits>::contains(const key_type& value) const {
+  if (size_ == 0) {
+    return false;
+  }
+  const size_t preferred_bucket = buckets_.H1(value);
+  const size_t h2 = buckets_.H2(value);
+  const size_t distance = buckets_[preferred_bucket].search_distance;
+  for (size_t i = 0; i <= distance; ++i) {
+    assert(preferred_bucket + i < buckets_.physical_size());
+    const Bucket<Traits>& bucket = buckets_[preferred_bucket + i];
+    // TODO: Use vector instructions to replace this loop.
+    for (size_t j = 0; j < Traits::kSlotsPerBucket; ++j) {
+      if (bucket.h2[j] == h2 && bucket.slots[j].value == value) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+template <class Traits>
+void HashTable<Traits>::rehash(size_t slot_count) {
+  Buckets<Traits> buckets(ceil(slot_count, Traits::kSlotsPerBucket));
+  buckets.swap(buckets_);
+  size_ = 0;
+  for (Bucket<Traits>& bucket : buckets) {
+    for (size_t j = 0; j < Traits::kSlotsPerBucket; ++j) {
+      if (bucket.h2[j] != Traits::kEmpty) {
+        // TODO: Take advantage of the fact that the value isn't in the table.
+        insert(std::move(bucket.slots[j].value));
+        // TODO: Destruct bucket.slots[j].
+      }
+    }
+  }
+}
+
+template <class Traits>
+void HashTable<Traits>::reserve(size_t count) {
+  // If the logical capacity * 7 /8 < count  then rehash
+  if (NeedsRehash(count)) {
+    // Set the LogicalSlotCount to at least count.  Don't grow by less than 1/7.
+    rehash(std::max(count, LogicalSlotCount() * 8 / 7));
+  }
+}
+
+template <class Traits>
+bool HashTable<Traits>::NeedsRehash(size_t target_size) const {
+  return LogicalSlotCount() * 7 < target_size * 8;
+}
+
+template <class Traits>
+size_t HashTable<Traits>::LogicalSlotCount() const {
+  return buckets_.logical_size() * Traits::kSlotsPerBucket;
+}
 
 }  // namespace yobiduck::internal
 

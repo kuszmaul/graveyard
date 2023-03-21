b52bf7b0fec0fc9d0628405786c0497f80f5f50e
Bradley C. Kuszmaul <kuszmaul@gmail.com>
2023-03-21 10:03:10 -0400

    clang-format gardening

--- internal/probe_length_benchmark.cc
+++ internal/probe_length_benchmark.cc
@@ -3,9 +3,9 @@
 #include <iostream>
 #include <random>
 
-#include "absl/hash/hash.h"       // for Hash
+#include "absl/hash/hash.h" // for Hash
 #include "graveyard_set.h"
-#include "internal/hash_table.h"  // for ProbeStatistics, Buckets
+#include "internal/hash_table.h" // for ProbeStatistics, Buckets
 
 int main(int argc, char *argv[]) {
   constexpr size_t kSize = 10'000'000;

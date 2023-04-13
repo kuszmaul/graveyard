7b872ad325e114597a73c6fb736435feb83b455d
Bradley C. Kuszmaul <kuszmaul@gmail.com>
2023-04-12 21:46:31 -0400

    Better job on tombstone insert-only workload probelength measurements

--- paper/experiments/README.md
+++ paper/experiments/README.md
@@ -17,17 +17,39 @@ that doesn't.  Here's how to read the data. Look at the first line
 which says
 
 ```
-90% full, no tombstones, add 2.5%(Knuth predicts 7.16667 89.3889):found=6.94481 notfound=84.3546
+90% full, no tombstones, add 2.5% (Knuth predicts 7.16667 89.3889):found=7.12803 notfound=5.71245 findempty=89.3774
 ```
 
-This means that we created a table that was 90% full with no tombstones. Then we added 2.5% more (so the table would be 92.5% full). Then we measured the average probe length for a successful lookup ("found=6.94481"), an insert (84.3546). (An insert is the same as a notfound). In this case, the table is 92.5% full, so X=13.33.
+This means that we created a table that was 90% full with no
+tombstones. Then we added 2.5% more (so the table would be 92.5%
+full). Then we measured the average probe length for a successful
+lookup ("found=7.12803"), for an unsuccessful lookup
+("notfound=5.71245") and to find an empty slot ("findempty=89.3774").
+To do an insert in general you must first do a notfound and then find
+an empty slot.  However for an insertion-only workload you don't have
+to do the notfound search first, since there won't be any empty slots
+before the end of the run.  In this case, the table is 92.5% full, so
+X=13.33.
 
- * Knuth predicts that found will cost (X+1)/2, which is 7.17.  (So 6.94 seems about right).
+ * Knuth predicts that found will cost $(X+1)/2$, which is 7.17.  (So
+   7.12803 seems about right).
 
- * Knuth predicts that notfound will cost (X^2+1)/2. (This is not
-   *ordered* linear probing, so we don't get to stop looking
-   early). Knuth predicts that notfound would cost 89.39, and the
-   measured number is 84.35.
+ * Knuth preducts that notfound will also cost $(X+1)/2$, which is
+   5.71245, which also sounds reasonable. Keep in mind that this is
+   not ordered linear probing, which would allow us to stop looking
+   early.  Instead, once a value is placed, we leave it there.  But we
+   do keep track of the distance that must be searched for every hash,
+   so we *do* get to stop looking early.  It's interesting that the
+   notfound searches actually cost less than the found searches.
+   That's because if a slot has never been used, we don't need to
+   probe at all.
+
+ * Knuth predicts that finding an empty slot will cost $(X^2+1)/2$.
+   In this cas that's 89.3889, and the simulated value is 89.3774
+   which is spookily accurate in this case.  It's less accurate in the
+   other cases with no tombstones.  For the case with tombstones,
+   Knuth's predictions are shown so we can see how much better
+   graveyard hashing does.
 
 The second line shows what happens if we inserted 5% tombstones at the
 rehash. So the table was 90% full with 5% of the tombstones spread
@@ -35,28 +57,40 @@ evenly, and 5% empties.
 
 (TODO: Run the experiment 20 times and produce confidence intervals).
 
-(TODO: I need to fix a little bug in the way not-found distances are
-calculated. This will increase the not-found cost slightly) Here's the
-data (analysis follows):
-
 ```shell
 $ make insert-only  && ./insert-only
-90% full, no tombstones, add 2.5%(Knuth predicts 7.16667 89.3889):found=6.94481 notfound=84.3546
-90% full, 5% tombstones, add 2.5%(Knuth predicts 7.16667 89.3889):found=10.6359 notfound=21.4327
-94% full, no tombstones, add 2% (Knuth predicts 13 313):found=12.9758 notfound=297.878
-94% full, 4% tombstones, add 2% (Knuth predicts 13 313):found=26.0294 notfound=36.2858
-96% full, no tombstones, add 1% (Knuth predicts 17.1667 556.056):found=17.2412 notfound=521.558
-96% full, 2% tombstones, add 1% (Knuth predicts 17.1667 556.056):found=25.5589 notfound=67.0138
-97% full, no tombstones, add 1% (Knuth predicts 25.5 1250.5):found=23.8672 notfound=1041.33
-97% full, 2% tombstones, add 1% (Knuth predicts 25.5 1250.5):found=45.9451 notfound=79.585
-98% full, no tombstones, add 0.5% (Knuth predicts 33.8333 2222.72):found=36.0105 notfound=3234.19
-98% full, 1% tombstones, add 0.5% (Knuth predicts 33.8333 2222.72):found=60.3546 notfound=151.658
-98% full, 0.8% tombstones, add 0.5% (Knuth predicts 33.8333 2222.72):found=48.6077 notfound=265.884
-98% full, 0.625% tombstones, add 0.5% (Knuth predicts 33.8333 2222.72):found=41.1458 notfound=583.304
-98% full, 0.5% tombstones, add 0.5% (Knuth predicts 33.8333 2222.72):found=37.8825 notfound=2085.02
-98% full, 0.4% tombstones, add 0.5% (Knuth predicts 33.8333 2222.72):found=37.1492 notfound=2616.25
+90% full, no tombstones, add 2.5% (Knuth predicts 7.16667 89.3889):found=7.12803 notfound=5.71245 findempty=89.3774
+90% full, 5% tombstones, add 2.5%       (Knuth predicts 7.16667 89.3889):found=11.0221 notfound=9.10819 findempty=22.0122
+94% full, no tombstones, add 2%         (Knuth predicts 13 313):found=12.9918 notfound=11.307 findempty=297.798
+94% full, 4% tombstones, add 2%         (Knuth predicts 13 313):found=25.1302 notfound=22.6016 findempty=36.5604
+96% full, no tombstones, add 1%         (Knuth predicts 17.1667 556.056):found=17.2439 notfound=15.4212 findempty=510.938
+96% full, 2% tombstones, add 1%         (Knuth predicts 17.1667 556.056):found=25.3078 notfound=23.0235 findempty=68.2908
+97% full, no tombstones, add 1%         (Knuth predicts 25.5 1250.5):found=24.6617 notfound=22.6645 findempty=1069.98
+97% full, 2% tombstones, add 1%         (Knuth predicts 25.5 1250.5):found=44.0987 notfound=41.3935 findempty=81.2281
+98% full, no tombstones, add 0.5%       (Knuth predicts 33.8333 2222.72):found=31.8208 notfound=29.7285 findempty=1706.71
+98% full, 1% tombstones, add 0.5%       (Knuth predicts 33.8333 2222.72):found=44.2069 notfound=41.7216 findempty=150.722
+98% full, 0.8% tombstones, add 0.5%     (Knuth predicts 33.8333 2222.72):found=38.7417 notfound=36.3969 findempty=272.671
+98% full, 0.625% tombstones, add 0.5%   (Knuth predicts 33.8333 2222.72):found=35.1971 notfound=32.9514 findempty=566.481
+98% full, 0.5% tombstones, add 0.5%     (Knuth predicts 33.8333 2222.72):found=33.555 notfound=31.363 findempty=997.569
+98% full, 0.4% tombstones, add 0.5%     (Knuth predicts 33.8333 2222.72):found=33.042 notfound=30.8714 findempty=1236.05
 ```
 
-Analysis: At 90% the value of graveyard for inserts is starting to show up, by 94% it's strong and at 98% it' huge.
+### Analysis
 
-Also: We really do need to insert 2x as many tombstones as we do inserts.
+At 90%+2.5%: graveyard hashing costs a little more for lookups (7.12
+vs. 11.02 for found; 5.7 vs. 9.1 for notfound).  But the cost of
+insertion is about a factor of 4 (89.4 vs 22.0).
+
+At 94%+2%: graveyard queries are costing a factor of two (because
+tombstones have halved the amount free space that was available for
+the first 94%).  Insertions are improved by a factor of 8.
+
+At a very high load factor (98% + 0.5%): THe queries are 50% slower
+and the inserts are 21 times faster.
+
+The last four lines show what happens if we don't put in enough
+tombstone.  Normally if we want to add $k$ values before rehasing, we
+put in $2k$ grarveyard tombstones.  (Hence when we were adding 2.5%,
+we put in 5% tombstones).  The last four lines show what happens if
+there are fewer tombstonew: the insert cost grows substantially.  Even
+removing 20% of the tombstones makes insertion nearly twice as slow.
--- paper/experiments/insert-only.cc
+++ paper/experiments/insert-only.cc
@@ -47,13 +47,17 @@ class Ratio {
   uint64_t numerator_, denominator_;
 };
 
-// Get a non-zero random number that hasn't been used before.
+struct Slot {
+  Slot() :empty(1) ,search_distance(0) ,value(0) {}
+  uint64_t empty : 1;
+  uint64_t search_distance : 63;
+  uint64_t value;
+};
+
+// Get a 64-bit random number that hasn't been used before.
 static uint64_t GetNumber(std::mt19937_64 &generator, std::uniform_int_distribution<uint64_t>& distribution, std::unordered_set<uint64_t>& seen_before) {
   while (true) {
     uint64_t v = distribution(generator);
-    if (v == 0) {
-      continue;
-    }
     auto [it, inserted] = seen_before.insert(v);
     if (inserted) {
       return v;
@@ -62,33 +66,33 @@ static uint64_t GetNumber(std::mt19937_64 &generator, std::uniform_int_distribut
 }
 
 struct ProbeStatistics {
-  Ratio found, notfound;
+  Ratio found, notfound, findempty;
   friend std::ostream& operator<<(std::ostream& os, const ProbeStatistics& stats) {
-    return os << "found=" << stats.found.Float() << " notfound=" << stats.notfound.Float();
+    return os << "found=" << stats.found.Float() << " notfound=" << stats.notfound.Float() << " findempty=" << stats.findempty .Float();
   }
 };
 
-// This set uses 0 as "empty".
 class IntSet {
  public:
-  IntSet(size_t n_slots) :slots_(n_slots, 0) {
+  IntSet(size_t n_slots) :slots_(n_slots, Slot{}) {
     // std::cout << "Initializing to slots=" << n_slots << std::endl;
   }
   // Requires: v is not in the set.
   void insert(uint64_t v, std::optional<size_t> graveyard_period) {
-    size_t h1 = v % slots_.size();
-    for (size_t off = 0; off < slots_.size(); ++off) {
-      size_t index = (h1 + off);
-      if (index >= slots_.size()) index -= slots_.size();
+    size_t h1 = H1(v);
+    size_t index = h1;
+    for (size_t off = 0; off < slots_.size(); ++off, IncrementIndex(index)) {
       if (graveyard_period.has_value() && index % *graveyard_period == 0) {
         // Skip a graveyard slot.
         continue;
       }
-      if (slots_[index] == 0) {
+      if (slots_[index].empty) {
         ++size_;
-        slots_[index] = v;
+        slots_[index].empty = 0;
+        slots_[index].value = v;
+        slots_[h1].search_distance = std::max(slots_[h1].search_distance, off + 1);
         return;
-      } else if (slots_[index] == v) {
+      } else if (slots_[index].value == v) {
         abort();
       }
     }
@@ -97,36 +101,50 @@ class IntSet {
   }
   ProbeStatistics GetProbeStatistics() const {
     size_t find_cost = 0;
-    for (uint64_t v : slots_) {
-      if (v != 0) {
-        find_cost += FoundCost(v);
+    for (Slot slot : slots_) {
+      if (!slot.empty) {
+        find_cost += FoundCost(slot.value);
       }
     }
     size_t notfound_cost = 0;
+    size_t find_empty_cost = 0;
     for (size_t i = 0; i < slots_.size(); ++i) {
+      // std::cout << "slot " << i << " " << NotFoundCost(i) << std::endl;
       notfound_cost += NotFoundCost(i);
+      find_empty_cost += FindEmptyCost(i);
     }
     return {.found = Ratio{find_cost, size_},
-            .notfound = Ratio{notfound_cost, slots_.size()}};
+            .notfound = Ratio{notfound_cost, slots_.size()},
+            .findempty = Ratio{find_empty_cost, slots_.size()}};
   }
  private:
+  void IncrementIndex(size_t &index) const {
+    ++index;
+    if (index == slots_.size()) {
+      index = 0;
+    }
+  }
+
+  size_t H1(uint64_t v) const {
+    return size_t((__int128(v) * __int128(slots_.size())) >> 64);
+  }
+
   friend std::ostream& operator<<(std::ostream &os, const IntSet &set) {
-    os << "{";
+    os << "{size=" << set.slots_.size() << " ";
     for (size_t i = 0; i < set.slots_.size(); ++i) {
       if (i > 0) os << ", ";
-      if (set.slots_[i] == 0) os << "_";
-      else os << set.slots_[i] % 1000;
+      if (set.slots_[i].empty) os << "_";
+      else os << set.H1(set.slots_[i].value);
     }
     return os << "}";
   }
 
   size_t FoundCost(uint64_t v) const {
-    assert(v != 0);
-    size_t h1 = v % slots_.size();
-    for (size_t off = 0; off < slots_.size(); ++off) {
-      size_t index = (h1 + off);
-      if (index >= slots_.size()) index -= slots_.size();
-      if (slots_[index] == v) {
+    size_t h1 = H1(v);
+    size_t index = h1;
+    for (size_t off = 0; off < slots_.size(); ++off, IncrementIndex(index)) {
+      const Slot& slot = slots_[index];
+      if (!slot.empty && slot.value == v) {
         return off + 1;
       }
     }
@@ -135,19 +153,22 @@ class IntSet {
   }
 
   size_t NotFoundCost(size_t i) const {
-    for (size_t off = 0; off < slots_.size(); ++off) {
-      size_t index = (i + off);
-      if (index >= slots_.size()) index -= slots_.size();
-      if (slots_[index] == 0) {
+    return slots_[i].search_distance;
+  }
+
+  size_t FindEmptyCost(size_t i) const {
+    size_t index = i;
+    for (size_t off = 0; off < slots_.size(); ++off, IncrementIndex(index)) {
+      if (slots_[index].empty) {
         return off + 1;
       }
     }
-    std::cerr << "All full" << std::endl;
+    std::cerr << "No empties" << std::endl;
     abort();
   }
 
   size_t size_ = 0;
-  std::vector<uint64_t> slots_;
+  std::vector<Slot> slots_;
 };
 
 void Measure(std::string_view description, std::optional<size_t> graveyard_period, Ratio load_factor, Ratio additional_load_factor, std::mt19937_64 generator) {
@@ -176,25 +197,26 @@ void Measure(std::string_view description, std::optional<size_t> graveyard_perio
   }
   // std::cout << "Then " << set << std::endl;
   ProbeStatistics stats = set.GetProbeStatistics();
-  std::cout << description << "(Knuth predicts " << xp1o2 << " " << x2p1o2 << "):" << stats << std::endl;
+  std::cout << description << " (Knuth predicts " << xp1o2 << " " << x2p1o2 << "):" << stats << std::endl;
 }
 
 int main() {
   // Generate the same sequence of random numbers for both tables.
   std::mt19937_64 gen;
+  //Measure("test", 10, Ratio{3, 5}, Ratio{1, 5}, gen);
   Measure("90% full, no tombstones, add 2.5%", std::nullopt, Ratio{90, 100}, Ratio{25, 1000}, gen);
   // 5% graveyard, 90% full
-  Measure("90% full, 5% tombstones, add 2.5%", 20, Ratio{90, 100}, Ratio{25, 1000}, gen);
-  Measure("94% full, no tombstones, add 2%  ", std::nullopt, Ratio{94, 100}, Ratio{2, 100}, gen);
-  Measure("94% full, 4% tombstones, add 2%  ", 25, Ratio{94, 100}, Ratio{2, 100}, gen);
-  Measure("96% full, no tombstones, add 1%  ", std::nullopt, Ratio{96, 100}, Ratio{1, 100}, gen);
-  Measure("96% full, 2% tombstones, add 1%  ", 50, Ratio{96, 100}, Ratio{1, 100}, gen);
-  Measure("97% full, no tombstones, add 1%  ", std::nullopt, Ratio{97, 100}, Ratio{1, 100}, gen);
-  Measure("97% full, 2% tombstones, add 1%  ", 50, Ratio{97, 100}, Ratio{1, 100}, gen);
-  Measure("98% full, no tombstones, add 0.5%  ", std::nullopt, Ratio{98, 100}, Ratio{1, 200}, gen);
-  Measure("98% full, 1% tombstones, add 0.5%  ", 100, Ratio{98, 100}, Ratio{1, 200}, gen);
-  Measure("98% full, 0.8% tombstones, add 0.5%  ", 125, Ratio{98, 100}, Ratio{1, 200}, gen);
+  Measure("90% full, 5% tombstones, add 2.5%      ", 20, Ratio{90, 100}, Ratio{25, 1000}, gen);
+  Measure("94% full, no tombstones, add 2%        ", std::nullopt, Ratio{94, 100}, Ratio{2, 100}, gen);
+  Measure("94% full, 4% tombstones, add 2%        ", 25, Ratio{94, 100}, Ratio{2, 100}, gen);
+  Measure("96% full, no tombstones, add 1%        ", std::nullopt, Ratio{96, 100}, Ratio{1, 100}, gen);
+  Measure("96% full, 2% tombstones, add 1%        ", 50, Ratio{96, 100}, Ratio{1, 100}, gen);
+  Measure("97% full, no tombstones, add 1%        ", std::nullopt, Ratio{97, 100}, Ratio{1, 100}, gen);
+  Measure("97% full, 2% tombstones, add 1%        ", 50, Ratio{97, 100}, Ratio{1, 100}, gen);
+  Measure("98% full, no tombstones, add 0.5%      ", std::nullopt, Ratio{98, 100}, Ratio{1, 200}, gen);
+  Measure("98% full, 1% tombstones, add 0.5%      ", 100, Ratio{98, 100}, Ratio{1, 200}, gen);
+  Measure("98% full, 0.8% tombstones, add 0.5%    ", 125, Ratio{98, 100}, Ratio{1, 200}, gen);
   Measure("98% full, 0.625% tombstones, add 0.5%  ", 160, Ratio{98, 100}, Ratio{1, 200}, gen);
-  Measure("98% full, 0.5% tombstones, add 0.5%  ", 200, Ratio{98, 100}, Ratio{1, 200}, gen);
-  Measure("98% full, 0.4% tombstones, add 0.5%  ", 225, Ratio{98, 100}, Ratio{1, 200}, gen);
+  Measure("98% full, 0.5% tombstones, add 0.5%    ", 200, Ratio{98, 100}, Ratio{1, 200}, gen);
+  Measure("98% full, 0.4% tombstones, add 0.5%    ", 225, Ratio{98, 100}, Ratio{1, 200}, gen);
 }
